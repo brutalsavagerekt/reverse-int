42cdf417a946ac367bb92c2e397da6b310ee4565
brutalsavagerekt <just.bondarchuk@gmail.com>
2023-08-17 15:04:39 +0300

    feat: add reverse functionality

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+    // convert to sting and reverse
+    let output = n.toString().split('').reverse().join('');
+
+    return parseInt(output)
+
+
 }
